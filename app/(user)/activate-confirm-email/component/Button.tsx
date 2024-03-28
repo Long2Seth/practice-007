4ff1e0eebba25d3db5b62fa3bf9b8cddc4510a15
longpiseth2002 <stevkhat121212@gmail.com>
2024-03-28 14:02:11 +0700

    Update

--- app/(user)/activate-confirm-email/component/Button.tsx
+++ app/(user)/activate-confirm-email/component/Button.tsx
@@ -1,9 +1,10 @@
-import  Style  from './style.module.css';
+import Style from './style.module.css';
+
 type Props = {
     title : string;
     onClick ?: () => void;
     className ?: string;
-}
+};
 
 
 export default function Button({title,onClick,className}: Props) {
